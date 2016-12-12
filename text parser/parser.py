e6310712edea9f31433a90a02601f92d9cd23355
adesca <axb131330@utdallas.edu>
2016-12-12 09:36:59 -0600

    Improved pride flag generation

--- text parser/parser.py
+++ text parser/parser.py
@@ -1,7 +1,7 @@
 from collections import Counter
 import json
 import re
-from wordcloud import WordCloud
+from wordcloud import WordCloud, ImageColorGenerator
 from PIL import Image
 import numpy as np
 
@@ -14,39 +14,40 @@ def getBadWords():
             wordList.append(line.strip())
     return wordList
 
-def getWords(stringOWords):
-    badWords = getBadWords();
+def getWords(stringOWords,badWords):
     listOWords = stringOWords.lower().split(' ')
-    return set([word for word in listOWords if word not in badWords])
+    return set([word for word in listOWords if word not in badWords and "_" not in word])
 
 def main():
     c = Counter()
+    badWords = getBadWords()
     with open("text.jl") as f:
         for line in f:
             tempDict = json.loads(line)
             if tempDict["articleText"] is not None:
-                words = getWords((tempDict["articleText"]+" "+tempDict["title"][0]))
-            for word in words:
-                c[word] += 1
+                words = getWords((tempDict["articleText"]+" "+tempDict["title"][0]), badWords)
+                for word in words:
+                    c[word] += 1
     with open("articleLinks.jl") as f:
-        for index,line in enumerate(f):
-            if index > 314:
-                tempDict = json.loads(line)
-                if tempDict["Title"]:
-                    words = getWords(tempDict["Title"][0])
-                    for word in words:
-                        c[word] += 1
-    
+        for line in f.readlines()[314:]:
+            tempDict = json.loads(line)
+            if tempDict["Title"]:
+                words = getWords(tempDict["Title"][0], badWords)
+                for word in words:
+                    c[word] += 1
+
     wordCloudString = ""
     for element in c.elements():
         wordCloudString += element+" "
-		
+
     mask = np.array(Image.open("pride flag.png"))
-    wordcloud = WordCloud(mask=mask).generate(wordCloudString)
+
+    wordcloud = WordCloud(mask=mask,color_func = ImageColorGenerator(mask), prefer_horizontal = 1).generate(wordCloudString)
+
     image = wordcloud.to_image()
-    image.save("prideCloud.bmp")
+    image.save("prideCloud2.bmp")
     image.show()
-    
+
     print(c['transgender']);
 
 if __name__ == '__main__':
